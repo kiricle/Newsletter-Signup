59deac436a69d71ff2135a8353e31b9177d452ae
Kolotylo Kyrylo <kolot102@gmail.com>
2021-12-21 19:04:46 +0200

    sets up request and response to the get and post requests

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/', (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  console.log(firstName, lastName, email);
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
